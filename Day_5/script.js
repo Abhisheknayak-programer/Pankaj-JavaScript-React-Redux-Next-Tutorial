e3508371730599aae14eae2a8185359156b538d2
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2022-10-30 20:46:28 +0530

    JavaScript Basics

---
+++ Day_5/script.js
@@ -0,0 +1,34 @@
+// IF CONDITIONAL STATEMENT
+let pankajAge = 23;
+if (pankajAge > 18) {
+  console.log("Can Vote and can Drive");
+}
+
+// IF ELSE CONDITION
+pankajAge = 13;
+if (pankajAge > 18) {
+  console.log("Can Vote and can Drive");
+} else {
+  console.log("Wait for some more years");
+}
+
+// IF ELSE LADDER
+pankajAge = 18;
+if (pankajAge > 18) {
+  console.log("Can Vote and can Drive");
+} else if (pankajAge < 18) {
+  console.log("Wait for some more years");
+} else {
+  console.log("Your age is 18");
+}
+
+let favFood = "Dosa";
+if (favFood === "Pizza") {
+  console.log("Jaldi Mota ho jaogee");
+} else if (favFood === "Burger") {
+  console.log("Jaldi Chota ho jayega");
+} else if (favFood === "Dosa") {
+  console.log("Thick Thak Hai!");
+} else {
+  console.log("Nothing Favorite");
+}
